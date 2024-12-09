697c8564cfc47974b19968f557c6315879b6bd86
Sara Ali <saraakz9ali@gmail.com>
2024-12-09 14:20:46 +0500

    Create SquareCalculator.java

---
+++ SquareCalculator.java
@@ -0,0 +1,10 @@
+public class SquareCalculator {
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        int input = 5; // Example input
+        System.out.println("Square of " + input + " is: " + square(input));
+    }
+}
